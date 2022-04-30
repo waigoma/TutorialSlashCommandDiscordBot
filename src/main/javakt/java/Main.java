ea5da2636ba3a20ae93fd57945e301de5c6816ea
waigoma <waichi0302@gmail.com>
2022-05-01 01:14:16 +0900

    initial commit

---
+++ src/main/javakt/java/Main.java
@@ -0,0 +1,23 @@
+package java;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    private static final String TOKEN = "Discord bot のトークンを記述";
+
+    public static void main(String[] args) {
+        try {
+            //Login
+            JDABuilder.createLight(TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+                    .addEventListeners()
+                    .setActivity(Activity.playing("作業")) // "～をプレイ中" の ～の部分
+                    .build();
+        }catch (LoginException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/javakt/kt/MainKt.kt
@@ -0,0 +1,29 @@
+package kt
+
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.requests.GatewayIntent
+
+import javax.security.auth.login.LoginException
+
+class MainKt {
+    companion object {
+        private const val TOKEN = "Discord bot のトークンを記述"
+    }
+
+    fun first() {
+        try {
+            //Login
+            JDABuilder.createLight(TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+                .addEventListeners()
+                .setActivity(Activity.playing("作業")) // "～をプレイ中" の ～の部分
+                .build()
+        } catch (e: LoginException) {
+            e.printStackTrace()
+        }
+    }
+}
+
+fun main(){
+    MainKt().first()
+}
