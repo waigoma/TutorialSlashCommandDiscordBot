3470711e0e770d457ee88aaca9bceb755d0d9688
waigoma <waichi0302@gmail.com>
2022-05-01 02:40:26 +0900

    add slash command

--- src/main/javakt/javamain/Main.java
+++ src/main/javakt/javamain/Main.java
@@ -1,22 +1,42 @@
 package javamain;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
     private static final String TOKEN = "Discord bot のトークンを記述";
+    private static final String GUILD_ID = "Discord サーバーの ID を記述";
 
     public static void main(String[] args) {
         try {
-            //Login
-            JDABuilder.createLight(TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+            // Login 処理
+            JDA jda = JDABuilder.createLight(TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                     .addEventListeners()
                     .setActivity(Activity.playing("作業")) // "～をプレイ中" の ～の部分
                     .build();
-        }catch (LoginException e){
+
+            // ログインが完了するまで待つ
+            jda.awaitReady();
+
+            // 参加しているサーバーを ID から取得
+            Guild guild = jda.getGuildById(GUILD_ID);
+
+            // 登録するコマンドを作成
+            SlashCommandData testCommand = Commands.slash("test", "テストコマンド");
+
+            // コマンドを指定したサーバーに登録
+            guild.updateCommands()
+                    .addCommands(testCommand)
+                    .queue();
+
+        }catch (LoginException | InterruptedException e){
             e.printStackTrace();
         }
     }
--- src/main/javakt/ktmain/MainKt.kt
+++ src/main/javakt/ktmain/MainKt.kt
@@ -2,6 +2,7 @@ package ktmain
 
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.requests.GatewayIntent
 
 import javax.security.auth.login.LoginException
@@ -9,15 +10,31 @@ import javax.security.auth.login.LoginException
 class MainKt {
     companion object {
         private const val TOKEN = "Discord bot のトークンを記述"
+        private const val GUILD_ID = "Discord サーバーの ID を記述"
     }
 
     fun first() {
         try {
-            //Login
-            JDABuilder.createLight(TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+            // Login 処理
+            val jda = JDABuilder.createLight(TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners()
                 .setActivity(Activity.playing("作業")) // "～をプレイ中" の ～の部分
                 .build()
+
+            // ログインが完了するまで待つ
+            jda.awaitReady()
+
+            // 参加しているサーバーを ID から取得
+            val guild = jda.getGuildById(GUILD_ID)!!
+
+            // 登録するコマンドを作成
+            val testCommand = Commands.slash("test", "テストコマンド")
+
+            // 指定したサーバーにコマンドを登録
+            guild.updateCommands()
+                .addCommands(testCommand)
+                .queue()
+
         } catch (e: LoginException) {
             e.printStackTrace()
         }
